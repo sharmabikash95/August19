533711b83f2c583401698c5ba5ad7a825a9917a4
Bikash Sharma <sharmabikas95@gmail.com>
2023-08-19 09:15:02 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace August19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Change the apperance
+            Console.Title = "Skynet";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WindowHeight = 20;
+
+            // geta converstaion going 
+
+            Console.WriteLine("Hello, what's your name");
+
+            Console.ReadLine();
+
+            Console.WriteLine("My name is Bikash.\nI'm learning c sharp today.");
+
+            Console.WriteLine("What's your favourite color?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("My favourite color is green.\n I also like blue and black colors");
+            
+
+
+
+
+           
+            Console.ReadKey();
+        }
+    }
+}
